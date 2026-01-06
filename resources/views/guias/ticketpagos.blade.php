1836b3051c891ee709e2fb993bccdf6f99cc19b3
Robin Castillo <robin.castillo@catolica.edu.sv>
2026-01-06 11:56:24 -0600

    cambios en qr tabla

--- resources/views/guias/ticketpagos.blade.php
+++ resources/views/guias/ticketpagos.blade.php
@@ -125,7 +125,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
            <td rowspan="5" style="width: 35%; text-align: center; vertical-align: top;">
     <div id="qrTicket" style="padding: 10px;"> {!! DNS2D::getBarcodeHTML((string)($ticketact[0]->codigo ?? ''), 'QRCODE', 4, 4) !!}</div>
 </td>
-                <td></td>
+               
                 <td>SUBTOTAL</td>
                 <td style="text-align: left; padding-left: 25px;">${{ $ticketact[0]->persoi + $ticketact[0]->puntoi + $ticketact[0]->casili + $ticketact[0]->depari + $ticketact[0]->guias }}
                  
