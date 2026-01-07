48f0d826e792aa6e46b610381596c9bf63d5d9bd
Robin Castillo <robin.castillo@catolica.edu.sv>
2026-01-07 15:50:59 -0600

    cambios en formulario

--- resources/views/guias/crearguia.blade.php
+++ resources/views/guias/crearguia.blade.php
@@ -511,7 +511,7 @@ License: For each use you must have a valid license purchased only from above li
 														<!--begin::Input group-->
 														<div class="row fv-row ">
 
-														<div class="col-md-4 mb-4">
+														<div class="col-md-4 mb-8">
 																<!--begin::Input-->
 																
 																<select name="tipo" id="tipo" class="form-control form-control-solid placeholder-select">
